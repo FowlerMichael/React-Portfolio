d62d0083062addeedc5f1e75a2e915896a3e0934
Michael Fowler <redfaction8008@gmail.com>
2023-08-28 22:17:18 -0500

    added resume page

--- react-portfolio/src/components/pages/resume/resume.js
+++ react-portfolio/src/components/pages/resume/resume.js
@@ -9,28 +9,57 @@ const styles = {
         color: 'white',
         padding: '0 20px',
     },
-    content: {
-        padding: 20,
-        display: 'flex',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        flexDirection: 'column',
-        background: '#fafafa',
+    resume: {
+        background: '#f5f5f5',
+        padding: '0 20px',
+        color: 'black',
     }
 }
 
 
-export default function Resume(){
-    return(
-        <div className="Resume">
-            <div style={styles.heading}>
-                <h1>Resume</h1></div>
-                <div style={styles.content}>
-                    <p>
-                    asfafafdfsadgf sfg
-                    </p>
-                </div>
-        </div>
-    )
+ function Resume() {
+    return (
+        <div style={styles.heading}>Resume 
+    <div className="resume" style={styles.resume}>
+   
+      <h1>Michael Fowler</h1>
+      <p>Web Developer</p>
+    
 
+    <section className="summary">
+      <h2>Summary</h2>
+      <p>Passionate web developer with experience...</p>
+    </section>
+
+    <section className="experience">
+      <h2>Experience</h2>
+      <div className="experience-item">
+        <h5>Company Name</h5>
+        <p>Position - Date</p>
+        <p>Job description...</p>
+      </div>
+    </section>
+
+    <section className="education">
+      <h2>Education</h2>
+      <div className="education-item">
+        <h5>University Name</h5>
+        <p>Degree - Graduation Year</p>
+      </div>
+    </section>
+
+    <section className="skills">
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>HTML</li>
+        <li>CSS</li>
+      </ul>
+    </section>
+  </div>
+  </div>
+    );
 }
+
+export default Resume;
